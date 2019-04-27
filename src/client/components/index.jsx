89214e262baf32bfe4725085e2dcf8819d7e1e49
Woojae Park <wjp241@nyu.edu>
2019-04-27 18:43:53 -0400

    eslint edited

--- src/client/components/index.jsx
+++ src/client/components/index.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "./app";
+import React from 'react';
+import { render } from 'react-dom';
+import App from './app';
 // import { AppContainer } from 'react-hot-loader';
 // "build": "webpack --mode production --devtool cheap-source-map"
 
 const renderApp = () => {
-  render(<App />, document.getElementById("root"));
+  render(<App />, document.getElementById('root'));
 };
 
 renderApp();
 
 // Hot Module Replacement API
 if (module.hot) {
-  module.hot.accept("./app", () => {
+  module.hot.accept('./app', () => {
     renderApp();
   });
 }
