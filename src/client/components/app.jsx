bf53518444f6c47a9646d8b3d33eb3e338a720bd
Woojae Park <wjp241@nyu.edu>
2019-04-27 20:05:55 -0400

    fixed import error

--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "../styles/app.css";
 import { Provider } from "react-redux";
 import store from "./store";
-import Landing from "./child";
+import Landing from "./child.jsx";
 
 const App = () => (
   <Provider store={store}>
